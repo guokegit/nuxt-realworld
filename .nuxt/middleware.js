f6d6d405377e97ccb1425b8112adaf93e63fb10d
郑丹红 <zhengdh@yuanxin2015.com>
2021-05-29 22:18:34 +0800

    删除.nuxt

--- .nuxt/middleware.js
+++
@@ -1,9 +0,0 @@
-const middleware = {}
-
-middleware['authenticate'] = require('../middleware/authenticate.js')
-middleware['authenticate'] = middleware['authenticate'].default || middleware['authenticate']
-
-middleware['hadAuthenticate'] = require('../middleware/hadAuthenticate.js')
-middleware['hadAuthenticate'] = middleware['hadAuthenticate'].default || middleware['hadAuthenticate']
-
-export default middleware
